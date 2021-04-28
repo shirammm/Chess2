c130005dc951e96ce998672f8762d8883f6bd7dc
Danielbotnik <danielbotnik@gmail.com>
2021-04-28 15:52:09 +0300

    typos

--- src/Piece.java
+++ src/Piece.java
@@ -1,23 +1,26 @@
 
 public class Piece {
 
-	public static final int black = 0b0;
-	public static final int white = 0b1;
+	public static final int BLACK = 0;
+	public static final int WHITE = 1;
 	
-	public static final int pawn = 0b001;
-	public static final int rook = 0b010;
-	public static final int knight = 0b011;
-	public static final int bishop = 0b100;
-	public static final int queen = 0b101;
-	public static final int king = 0b111;
+	public static final int NONE = 0;
+	public static final int PAWN = 1;
+	public static final int KING = 2;
+	public static final int KNIGHT = 3;
+	public static final int BISHOP = 5;
+	public static final int QUEEN = 6;
+	public static final int ROOK = 7;
 	
+	public static final int pieceMask = 0b1110;
+	public static final int colorMask = 0b1;
 	
 	public static int pieceType(int piece){
-		return piece & 0b1110;
+		return piece & pieceMask;
 	}
 	
 	public static boolean pieceColor(int piece){
-		return (piece & 0b1) == 1;
+		return (piece & colorMask) == 1;
 	}
 	
 }
